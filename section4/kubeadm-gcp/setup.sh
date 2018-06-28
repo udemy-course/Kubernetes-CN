b192ba7abd08b96848c43088b75ea82fc3a91e77
Peng Xiao <xiaoquwl@gmail.com>
2018-06-28 15:12:34 +0800

    gcp kubeadm
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ section4/kubeadm-gcp/setup.sh
@@ -0,0 +1,33 @@
+#/bin/sh
+
+# install some tools
+sudo yum install -y vim telnet bind-utils wget
+
+sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF'
+
+# install and start docker
+sudo yum install -y docker
+sudo systemctl enable docker && sudo systemctl start docker
+# Verify docker version is 1.12 and greater.
+
+sudo setenforce 0
+
+# install kubeadm, kubectl, and kubelet.
+sudo yum install -y kubelet kubeadm kubectl
+
+sudo bash -c 'cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF'
+sudo sysctl --system
+
+sudo systemctl disable firewalld
+sudo swapoff -a
